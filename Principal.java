2b9c52714e70e16dcae819b91c17c080be29ae0c
Mauro-coder555 <maupereyra96@gmail.com>
2020-09-25 15:23:14 -0300

    sexto commit

--- Principal.java
+++ Principal.java
@@ -7,11 +7,10 @@ public class Principal {
 		
 		Auto miAuto;
 		
-<<<<<<< HEAD
+
 		miAuto = new Auto("Naranja",5);
-=======
-		miAuto = new Auto("Rojo",5);
->>>>>>> e7c88611d92c8547810a773ce8925f86f0d909d1
+
+
 		
 		System.out.println("Mi auto es: "+ miAuto.getColor());
 		
